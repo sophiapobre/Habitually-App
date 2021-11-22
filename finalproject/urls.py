53af5a674ed7d09673f557f056cdb03dee322fb3
Sophia Pobre <sophiapobre@gmail.com>
2021-11-22 18:25:21 +1100

    Added path to add_habit view

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+    path("add_habit", views.add_habit, name="add_habit")
 ]
