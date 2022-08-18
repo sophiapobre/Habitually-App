a491592a2f3f843c9649bce6a4a29dcb55128323
Sophia Pobre <sophiapobre@gmail.com>
2022-08-18 20:03:59 +0800

    Changed database  engine from SQLite to PostgreSQL

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'finalproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD':env('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
